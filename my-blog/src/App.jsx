560e55bd044afa0a2f6cb12549894bc4505c90b7
Muniraja <muniraja0799@gmail.com>
2022-05-06 16:20:39 +0530

    commit

---
+++ my-blog/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+const App = () => {
+  const title = "Hello Muniraja, welcome to React to Course";
+  const link = "https://github.com/muniraja0799";
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
