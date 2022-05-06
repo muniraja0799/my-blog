7cf207b2ea3c25947b9a876465a80d33f5f4fb66
Muniraja <muniraja0799@gmail.com>
2022-05-06 16:44:40 +0530

    commit

--- my-blog/src/App.jsx
+++ my-blog/src/App.jsx
@@ -1,12 +1,12 @@
-import "./App.css";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 const App = () => {
-  const title = "Hello Muniraja, welcome to React to Course";
-  const link = "https://github.com/muniraja0799";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        <Home />
       </div>
     </div>
   );
